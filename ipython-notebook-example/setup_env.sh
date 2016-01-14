6cde06306350687117866ccac0450865339b7cd6
Alexandra Leigh Johnson <alexandra@sigopt.com>
2016-01-14 11:52:00 -0800

    setup script

---
+++ ipython-notebook-example/setup_env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+OS_NAME="$(uname -s)"
+if [ '$OS_NAME' == 'Darwin' ]; then
+  brew install gfortran
+  sudo easy_install pip
+else
+  # assuming using ubuntu
+  sudo apt-get update
+  sudo apt-get -y install python-pip python-dev libopenblas-dev liblapack-dev gfortran
+fi
+sudo pip install -r requirements.txt
