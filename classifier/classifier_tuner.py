d15db66f9f2195cabcb065f5a539e5a0d3a14925
Patrick Hayes <pfhayes@gmail.com>
2016-06-07 18:16:11 -0700

    Update to sigopt 1.6.0

--- classifier/classifier_tuner.py
+++ classifier/classifier_tuner.py
@@ -111,7 +111,7 @@ class ExampleRunner(object):
             )
         except ApiException as e:
             if e.status_code == 403 and 'support@sigopt.com' in str(e):
-                existing_experiments = conn.experiments().fetch().data
+                existing_experiments = list(conn.experiments().fetch().iterate_pages())
                 if existing_experiments:
                     raise Exception(
                         "You have existing experiments on sigopt.com: {0}."
