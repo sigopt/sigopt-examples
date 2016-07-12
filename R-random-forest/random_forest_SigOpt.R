58a640ff2bb21ee6cea739bf0b54bc06655bd96b
Alexandra Leigh Johnson <alexandra@sigopt.com>
2016-07-12 16:46:43 -0700

    Some cleanup: remove train/validate/test and have only train/validate for brevity, some style and commenting cleanup

--- R-random-forest/random_forest_SigOpt.R
+++ R-random-forest/random_forest_SigOpt.R
@@ -1,4 +1,8 @@
-# install packages
+# Use SigOpt to tune a Random Forest Classifier in R
+# Learn more about SigOpt's R Client:
+# https://sigopt.com/docs/overview/R
+
+# Install packages
 install.packages("devtools", repos = "http://cran.us.r-project.org")
 library(devtools)
 install_github("sigopt/SigOptR")
@@ -6,22 +10,19 @@ library(SigOptR)
 install.packages('randomForest', repos = "http://cran.us.r-project.org")
 library(randomForest)
 
-# Learn more about authenticating the SigOpt API: 
+
+# Learn more about authenticating the SigOpt API:
 # https://sigopt.com/docs/overview/authentication
 Sys.setenv(SIGOPT_API_TOKEN="INSERT_YOUR_TOKEN_HERE")
 
-# load dataset
-# we are using iris dataset as an example
-library(datasets)
-ind <- sample(2,nrow(iris),replace=TRUE,prob=c(0.7,0.3))
-trainData <- iris[ind==1,]
-testData <- iris[ind==2,]
-X_train <- subset(trainData, select = -c(Species))
-y_train <- trainData$Species
-X_test <- subset(testData, select = -c(Species))
-y_test <- testData$Species
 
-# Create an RF tuning experiment
+# Load dataset
+# We are using the iris dataset as an example
+library(datasets)
+X <- subset(iris, select = -c(Species))
+y <- iris$Species
+
+# Create a SigOpt experiment for the Random Forest parameters
 experiment <- create_experiment(list(
   name="R RandomForest",
   parameters=list(
@@ -31,52 +32,60 @@ experiment <- create_experiment(list(
     list(name="nodesize", type="int", bounds=list(min=1, max=10))
   )
 ))
+print(paste("Created experiment: https://sigopt.com/experiment", experiment$id, sep="/"))
 
-CV_accuracy <- function(assignments, X_train, y_train){
+# Our object metric is the mean of cross validated accuracies
+# We use cross validation to prevent overfitting
+evaluate_model <- function(assignments, X, y) {
   # evaluate cross folds for accuracy
   num_folds = 5
   cv_accuracies = c()
   for (k in 1:num_folds){
-    ind <- sample(2,nrow(trainData),replace=TRUE,prob=c(1-1.0/num_folds,1.0/num_folds))    
-    X_cv_train = X_train[ind==1,]
-    y_cv_train = y_train[ind==1]
-    X_cv_valid = X_train[ind==2,]
-    y_cv_valid = y_train[ind==2]
-    
-    rf_fit <- randomForest(y=y_cv_train,x=X_cv_train,
-                           ntree=assignments$ntree,mtry=assignments$mtry,
-                           sampsize=assignments$sampsize*nrow(X_cv_train),nodesize=assignments$nodesize,
+    cv_split <- sample(2, nrow(iris), replace=TRUE, prob=c(1-1.0/num_folds, 1.0/num_folds))
+    X_train <- X[cv_split==1, ]
+    y_train <- y[cv_split==1]
+    X_valid <- X[cv_split==2, ]
+    y_valid <- y[cv_split==2]
+
+    rf_fit <- randomForest(y=y_train,
+                           x=X_train,
+                           ntree=assignments$ntree,
+                           mtry=assignments$mtry,
+                           sampsize=assignments$sampsize*nrow(X_train),
+                           nodesize=assignments$nodesize,
                            proximity=TRUE)
-    prediction <- predict(rf_fit, X_cv_valid)
-    rightPred <- prediction == y_cv_valid
-    accuracy <- sum(rightPred)/nrow(X_cv_valid)
+    prediction <- predict(rf_fit, X_valid)
+    correct_predictions <- prediction == y_valid
+    accuracy <- sum(correct_predictions)/nrow(X_valid)
     cv_accuracies <- c(accuracy, cv_accuracies)
   }
-  return(c(mean(cv_accuracies),sd(cv_accuracies)))
+  return(c(mean(cv_accuracies), sd(cv_accuracies)))
 }
 
-best_assignment = NULL
-best_val = NULL
-for (i in 1:80){
-  # Retrieve a suggestion
+# Run the Optimization Loop between 10x - 20x the number of parameters
+for (i in 1:80) {
+  # Receive a Suggestion from SigOpt
   suggestion <- create_suggestion(experiment$id)
-  # Evaluate cross validated accuracy
-  res <- CV_accuracy(suggestion$assignments, X_train, y_train)
-  if (is.null(best_val) || res[1] > best_val  ) {
-    best_val <- res[1]
-    best_assignment <- suggestion$assignments
-  }
-  # Report observation back to SigOpt
-  create_observation(experiment$id, list(suggestion=suggestion$id, value=res[1], value_stddev=res[2]))
+
+  # Evaluate the model locally
+  res <- evaluate_model(suggestion$assignments, X, y)
+
+  # Report an Observation (with standard deviation) back to SigOpt
+  create_observation(experiment$id, list(suggestion=suggestion$id,
+                                         value=res[1],
+                                         value_stddev=res[2]))
 }
 
-# get the best observations of the experiment 
-# evaluate on test set
-rf_fit <- randomForest(y=y_train,x=X_train,ntree=best_assignment$ntree,
-                       mtry=1, sampsize=best_assignment$sampsize*nrow(X_train),
-                       nodesize=best_assignment$nodesize, proximity=TRUE)
-prediction <- predict(rf_fit, X_test)
-rightPred <- prediction == y_test
-accuracy <- sum(rightPred)/nrow(X_test)
-print(accuracy)
+# Re-fetch the experiment to get the best observed value and assignments
+experiment <- fetch_experiment(experiment$id)
+best_assignments <- experiment$progress$best_observation$assignments
 
+# To wrap up the Experiment, fit the RandomForest on the best assigments
+# and train on all available data
+rf <- randomForest(x=X,
+                   y=y,
+                   ntree=best_assignments$ntree,
+                   mtry=best_assignments$mtry,
+                   sampsize=best_assignments$sampsize*nrow(X),
+                   nodesize=best_assignments$nodesize,
+                   proximity=TRUE)
