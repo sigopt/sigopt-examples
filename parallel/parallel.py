2420c9e4abd046b50fdb613ff6d713a944c9eb89
Alexandra Leigh Johnson <alexandra@sigopt.com>
2016-12-20 17:05:56 -0800

    small cleanup

--- parallel/parallel.py
+++ parallel/parallel.py
@@ -1,15 +1,40 @@
-import argparse
-import math
 import threading
-
 from sigopt import Connection
-
 from data import PARAMETERS, evaluate_model
 
 # You can find your API token at https://sigopt.com/docs/overview/authentication
 SIGOPT_API_KEY = 'YOUR_API_TOKEN_HERE'
 
 NUM_WORKERS = 2
+
+class Master(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    self.conn = Connection(client_token=SIGOPT_API_KEY)
+    experiment = self.conn.experiments().create(
+      name='Parallel Experiment',
+      parameters=PARAMETERS,
+      observation_budget=len(PARAMETERS) * 20,
+      metadata=dict(num_workers=NUM_WORKERS),
+    )
+    print("View your experiment progress: https://sigopt.com/experiment/{}".format(experiment.id))
+    self.experiment_id = experiment.id
+
+  @property
+  def remaining_observations(self):
+    experiment = self.conn.experiments(self.experiment_id).fetch()
+    return experiment.observation_budget - experiment.progress.observation_count
+
+  def run(self):
+    tries = 3
+    while (tries > 0 and self.remaining_observations > 0):
+      workers = [Worker(self.experiment_id) for _ in xrange(NUM_WORKERS)]
+      for worker in workers:
+        worker.start()
+      for worker in workers:
+        worker.join()
+      self.conn.experiments(self.experiment_id).suggestions().delete(state='open')
+      tries -= 1
 
 class Worker(threading.Thread):
   def __init__(self, experiment_id):
@@ -41,35 +66,6 @@ class Worker(threading.Thread):
         failed=failed,
         metadata=self.metadata,
       )
-
-class Master(threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-    self.conn = Connection(client_token=SIGOPT_API_KEY)
-    experiment = self.conn.experiments().create(
-      name='Parallel Experiment',
-      parameters=PARAMETERS,
-      observation_budget=len(PARAMETERS) * 20,
-      metadata=dict(num_workers=NUM_WORKERS),
-    )
-    print("View your experiment progress: https://sigopt.com/experiment/{}".format(experiment.id))
-    self.experiment_id = experiment.id
-
-  @property
-  def remaining_observations(self):
-    experiment = self.conn.experiments(self.experiment_id).fetch()
-    return experiment.observation_budget - experiment.progress.observation_count
-
-  def run(self):
-    tries = 3
-    while (tries > 0 and self.remaining_observations > 0):
-      workers = [Worker(self.experiment_id) for _ in xrange(NUM_WORKERS)]
-      for worker in workers:
-        worker.start()
-      for worker in workers:
-        worker.join()
-      self.conn.experiments(self.experiment_id).suggestions().delete(state='open')
-      tries -= 1
 
 if __name__ == '__main__':
   master = Master()
