ce499400cb85ae2e47924187efb0151fd83ba220
Ben Hsu <benhsu@Bens-MacBook-Pro.local>
2017-06-13 19:33:20 -0700

    Refactor code

--- caffe2-cnn/mnist_nn_cpu.py
+++ caffe2-cnn/mnist_nn_cpu.py
@@ -3,11 +3,12 @@ import math
 import numpy as np
 import os
 import shutil
+import subprocess
 import urllib2
 from caffe2.python import workspace, core, cnn, net_drawer, visualize
 from sigopt import Connection
 
-current_folder = os.path.join(os.path.expanduser('~'), 'caffe2_notebooks')
+current_folder = os.path.join(os.getcwd(), 'caffe2_mnist')
 
 data_folder = os.path.join(current_folder, 'tutorial_data', 'mnist')
 root_folder = os.path.join(current_folder, 'tutorial_files', 'tutorial_mnist')
@@ -16,10 +17,11 @@ label_file_train = os.path.join(data_folder, "train-labels-idx1-ubyte")
 image_file_test = os.path.join(data_folder, "t10k-images-idx3-ubyte")
 label_file_test = os.path.join(data_folder, "t10k-labels-idx1-ubyte")
 
+conn = Connection(client_token='SIGOPT_API_TOKEN')
+
 # Get the dataset if it is missing
 def download_dataset(url, path):
   import requests, zipfile, StringIO
-  print "Downloading... ", url, " to ", path
   r = requests.get(url, stream=True)
   z = zipfile.ZipFile(StringIO.StringIO(r.content))
   z.extractall(path)
@@ -27,16 +29,9 @@ def download_dataset(url, path):
 # Load training/testing data into db
 def generate_db(image, label, name):
   name = os.path.join(data_folder, name)
-  print 'DB: ', name
   if not os.path.exists(name):
     syscall = "/usr/local/binaries/make_mnist_db --channel_first --db leveldb --image_file " + image + " --label_file " + label + " --output_file " + name
-    # print "Creating database with: ", syscall
-    os.system(syscall)
-  else:
-    print "Database exists already. Delete the folder if you have issues/corrupted DB, then rerun this."
-    if os.path.exists(os.path.join(name, "LOCK")):
-      # print "Deleting the pre-existing lock file"
-      os.remove(os.path.join(name, "LOCK"))
+    subprocess.check_call(syscall, shell=True)
 
 def data_setup():
   if not os.path.exists(data_folder):
@@ -45,7 +40,6 @@ def data_setup():
     download_dataset("https://s3.amazonaws.com/caffe2/datasets/mnist/mnist.zip", data_folder)
 
   if os.path.exists(root_folder):
-    print("Looks like you ran this before, so we need to cleanup those old files...")
     shutil.rmtree(root_folder)
 
   os.makedirs(root_folder)
@@ -141,26 +135,19 @@ def train_model(model):
   total_iters = 100
   for i in range(total_iters):
     workspace.RunNet(model.net.Proto().name)
-    if(i % 5 == 0):
-      print("Iteration " + str(i) + " - Accuracy: " + str(workspace.FetchBlob('accuracy').item()))
 
   return workspace.FetchBlob('accuracy').item()
 
 # Loops through retrieving suggestion, evaluating model, and reporting observation
 def sigopt_optimization_loop(conn, experiment):
   for _ in range(experiment.observation_budget):
-    print('\n')
-    print("Optimization Loop #" + str(_))
-
     # Fetch new suggestion
     suggestion = conn.experiments(experiment.id).suggestions().create()
     assignments = suggestion.assignments
-    print(assignments)
 
     # Test suggestion
     model = create_model(assignments)
     accuracy = train_model(model)
-    print("Observed Accuracy: " + str(accuracy))
 
     # Report observation
     conn.experiments(experiment.id).observations().create(
@@ -172,7 +159,6 @@ def sigopt_optimization_loop(conn, experiment):
   experiment = conn.experiments(experiment.id).fetch()
   assignments = conn.experiments(experiment.id).best_assignments().fetch()
 
-  print('\n')
   print("Best Assignments:")
   print(assignments.data[0].assignments)
 
@@ -181,8 +167,6 @@ def sigopt_optimization_loop(conn, experiment):
 def main():
   # Downloads data if it doesn't exist yet
   data_setup()
-
-  conn = Connection(client_token='insert_token_here')
 
   # Create experiment in SigOpt portal
   experiment = setup_sigopt_experiment(conn)
