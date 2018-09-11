ef60f83b4ca082d37f28f9cf04b6ae413509fabd
Alexandra Johnson <alexandraj777@users.noreply.github.com>
2018-09-11 10:29:46 -0700

    Small fix and example (#100)
    * Small fix to power, and new example
    
    * Small fix to power, and new example

--- orchestrate/sgd_classifier/model_without_orchestrate.py
+++ orchestrate/sgd_classifier/model_without_orchestrate.py
@@ -1,6 +1,5 @@
-# SGDClassifier example using orchestrate.io
+# SGDClassifier example without orchestrate.io
 
-import orchestrate.io
 from sklearn import datasets
 from sklearn.linear_model  import SGDClassifier
 from sklearn.model_selection import cross_val_score
@@ -14,12 +13,12 @@ def load_data():
 
 def evaluate_model(X, y):
   classifier = SGDClassifier(
-    loss=orchestrate.io.assignment('loss', default='log'),
-    penalty=orchestrate.io.assignment('penalty', default='elasticnet'),
-    alpha=10**orchestrate.io.assignment('log_alpha', -4),
-    l1_ratio=orchestrate.io.assignment('l1_ratio', 0.15),
-    max_iter=orchestrate.io.assignment('max_iter', default=1000),
-    tol=orchestrate.io.assignment('tol', default=0.001),
+    loss='log',
+    penalty='elasticnet',
+    alpha=10**(-4),
+    l1_ratio=0.15,
+    max_iter=1000,
+    tol=0.001,
   )
   cv_accuracies = cross_val_score(classifier, X, y, cv=5)
   return (numpy.mean(cv_accuracies), numpy.std(cv_accuracies))
@@ -28,4 +27,3 @@ if __name__ == "__main__":
   (X, y) = load_data()
   (mean, std) = evaluate_model(X=X, y=y)
   print('Accuracy: {} +/- {}'.format(mean, std))
-  orchestrate.io.log_metric('accuracy', mean, std)
