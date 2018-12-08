28285d603c5fdbafde2bed9eb7788c574c3c0927
Nakul Pathak <nakul@sigopt.com>
2018-12-07 18:52:39 -0800

    Use assignment and log metric but always 4.0 result

--- orchestrate/spark_example/model.py
+++ orchestrate/spark_example/model.py
@@ -20,6 +20,7 @@
 
 from __future__ import print_function
 
+import orchestrate.io
 import sys
 from random import random
 from operator import add
@@ -40,11 +41,13 @@ if __name__ == "__main__":
     n = 100000 * partitions
 
     def f(_):
-        x = random() * 2 - 1
-        y = random() * 2 - 1
+        x = orchestrate.io.assignment('rand_num', 0.4) * 2 - 1
+        y = orchestrate.io.assignment('rand_num', 0.2) * 2 - 1
         return 1 if x ** 2 + y ** 2 <= 1 else 0
 
     count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-    print("Pi is roughly %f" % (4.0 * count / n))
+    curr_pi_val = (4.0 * count / n)
+    print("Pi is roughly %f" % curr_pi_val)
+    orchestrate.io.log_metric('accuracy', curr_pi_val)
 
     spark.stop()
